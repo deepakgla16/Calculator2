660dd89c26fa48a894074600291215168b66e2b4
deepakgla16 <deepaksharma972079@gmail.com>
2025-01-18 12:01:59 +0530

    my first commit add

---
+++ addition.java
@@ -0,0 +1,14 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class addition {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+
+        int a = 5;
+        int b = 5;
+
+        System.out.println(a-b);
+    }
+}
